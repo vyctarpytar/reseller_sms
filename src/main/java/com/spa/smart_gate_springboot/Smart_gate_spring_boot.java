50c6cdd0f18de9db285df55c9d4a38cba808a6a8
vyctarpytar <vyctarpytar@gmail.com>
2025-08-13 02:21:41 +0300

    start application

--- src/main/java/com/spa/smart_gate_springboot/Smart_gate_spring_boot.java
+++ src/main/java/com/spa/smart_gate_springboot/Smart_gate_spring_boot.java
@@ -1,5 +1,6 @@
 package com.spa.smart_gate_springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,9 +9,11 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@Slf4j
 public class Smart_gate_spring_boot {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Africa/Nairobi"));
+        log.info("----XXXXX-------Application started");
         SpringApplication.run(Smart_gate_spring_boot.class, args);
     }
 
