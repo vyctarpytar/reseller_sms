5e2ad328afe75565305455230904cd5d3805ed34
vyctarpytar <vyctarpytar@gmail.com>
2025-08-27 18:00:26 +0300

    Removed Bean dependencies 2

--- src/main/java/com/spa/smart_gate_springboot/account_setup/account/dtos/AcDelete.java
+++ src/main/java/com/spa/smart_gate_springboot/account_setup/account/dtos/AcDelete.java
@@ -1,9 +1,8 @@
 package com.spa.smart_gate_springboot.account_setup.account.dtos;
 
-import lombok.Builder;
+
 import lombok.Data;
 
-@Builder
 @Data
 public class AcDelete {
     private String acDeleteReason;
