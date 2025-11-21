942ab39dcfb85392404b8825f559e3bb2c5a4a00
vyctarpytar <vyctarpytar@gmail.com>
2025-11-21 08:00:57 +0300

    Added schedule on @AccountService to recreate API keys 1

--- src/main/java/com/spa/smart_gate_springboot/messaging/send_message/api/SeCuredApiController.java
+++ src/main/java/com/spa/smart_gate_springboot/messaging/send_message/api/SeCuredApiController.java
@@ -1,5 +1,6 @@
 package com.spa.smart_gate_springboot.messaging.send_message.api;
 
+import com.spa.smart_gate_springboot.dto.Layers;
 import com.spa.smart_gate_springboot.user.UserService;
 import com.spa.smart_gate_springboot.utils.StandardJsonResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v2/api-key")
@@ -20,8 +24,16 @@ public class SeCuredApiController {
 
     @GetMapping()
     @PreAuthorize("hasAnyRole('ACCOUNTANT','SUPER_ADMIN','ADMIN')")
-    public StandardJsonResponse getDevSandBox(HttpServletRequest request) {
+    public StandardJsonResponse getDevSandBox(HttpServletRequest request, @RequestParam String account_id) {
         var user = userService.getCurrentUser(request);
+        if (!user.getLayer().equals(Layers.ACCOUNT)) {
+            if (account_id == null) {
+                throw new RuntimeException("Account ID is required");
+            }
+            user = userService.findById(UUID.fromString(account_id));
+
+            return  apiKeyService.getAccountApiKeyInfo(user);
+        }
         return  apiKeyService.getAccountApiKeyInfo(user);
     }
 
